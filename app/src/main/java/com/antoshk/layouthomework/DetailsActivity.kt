689e8cb0d8158d426e4aad4dfd7bfa2f9a2f7004
a_shelkovich <a_shelkovich@todes.by>
2019-09-23 13:44:20 +0300

    initial commit

---
+++ app/src/main/java/com/antoshk/layouthomework/DetailsActivity.kt
@@ -0,0 +1,24 @@
+package com.antoshk.layouthomework
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+        initToTrailerButton()
+    }
+
+    private fun initToTrailerButton(){
+        val toTrailerButton: Button = findViewById(R.id.trailer_button)
+        toTrailerButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:6ZfuNTqbHE8"))
+            startActivity(intent)
+        }
+    }
+}
